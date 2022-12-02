fcf16b35c414de7f491ea6924732f0ac68080aa6
Lucas <lmrfreitas9@gmail.com>
2022-12-01 22:44:08 -0300

    create habit e estrutura de rotas

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -2,8 +2,10 @@ import { View, Text, ScrollView, TouchableOpacity, Image, StyleSheet } from 'rea
 import React, {useState} from 'react'
 import { useNavigation } from '@react-navigation/native'
 
-export default function HabitPage() {
+export default function HabitPage({route}) {
     const navigation = useNavigation()
+
+    const {create, habit} = route.params
 
   return (
     <View style={styles.container}>
@@ -15,7 +17,13 @@ export default function HabitPage() {
                 source={require('../../assets/icons/arrowBack.png')}
                 style={styles.arrowBack}/>
             </TouchableOpacity>
-            <View style={styles.mainContent}></View>
+            <View style={styles.mainContent}>
+                <Text style={styles.title}>Configurações{"\n"}de hábito</Text>
+                <Text style={styles.inputText}>Área</Text>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.area}>{habit?.habitArea}</Text>
+                </View>
+            </View>
         </View>
       </ScrollView>
     </View> 
@@ -34,11 +42,35 @@ const styles = StyleSheet.create({
     },
     arrowBack:{
         width: 40,
-        heigth: 40,
+        height: 40,
     },
     mainContent:{
         wdith: 250,
         alignSelf: 'center'
+    },
+    title:{
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 30
+    },
+    inputText:{
+        color: 'white',
+        fontSize: 16,
+        marginTop: 35,
+        marginBottom: 10,
+        marginLeft: 5
+    },
+    inputContainer:{
+        borderWidth: 1,
+        borderColor: "#FFFFFF",
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 15
+    },
+    area: {
+        color: '#BBBBBB',
+        fontSize: 15
     }
 
     })
